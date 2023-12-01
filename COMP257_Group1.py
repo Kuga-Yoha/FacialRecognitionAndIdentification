ec0e6da10bb28f03bcb34bf0f34b92ec0d71e96e
Kugavathanan Yohanathan <kugajpx2012@gmail.com>
2023-12-01 15:43:08 -0500

    add read data

--- COMP257_Group1.py
+++ COMP257_Group1.py
@@ -2,7 +2,38 @@
 """
 Created on Fri Dec  1 14:37:36 2023
 
-@author: Kugavathanan
+@author: Kugavathanan  Yohanathan
+         Sayanthana  Yoganathan
+         
+         COMP-257
 """
 
 
+
+import scipy.io as io
+import os
+
+
+#Get the current file location
+current_file_location = os.path.abspath(__file__)
+
+
+print("Current file location:", current_file_location)
+
+
+mat_file_path = os.path.join(os.path.dirname(current_file_location), 'umist_cropped.mat')
+
+#Load the .mat file
+mat_data = io.loadmat(mat_file_path)
+
+
+facedat_data = mat_data['facedat']
+
+#print(type(facedat_data))
+
+print(facedat_data.shape)
+#(1, 20)
+
+
+
+print(facedat_data[0].size)
